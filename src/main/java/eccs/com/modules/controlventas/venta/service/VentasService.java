9aa03f87f043f918f290d1ce53fe059e261cdf69
Corpsey69 <elizandro.carballo.casillas@eccs.com.mx>
2026-05-27 18:24:54 -0600

    ECCS - CONTROL VENTAS - Comprobante Venta
    -- comenzamos a crear el flujo de venta

--- src/main/java/eccs/com/modules/controlventas/venta/service/VentasService.java
+++ src/main/java/eccs/com/modules/controlventas/venta/service/VentasService.java
@@ -2,7 +2,9 @@ package eccs.com.modules.controlventas.venta.service;
 
 import eccs.com.core.dtos.ResponseDto;
 import eccs.com.modules.controlventas.venta.dto.CrearVentaRequestDto;
+import eccs.com.modules.controlventas.venta.dto.DataVentaRequestDto;
 
 public interface VentasService {
     public ResponseDto<Object> getCrearVenta(CrearVentaRequestDto request);
+    public ResponseDto<Object> getDataVenta(DataVentaRequestDto request);
 }
